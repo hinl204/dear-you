d74ce8e7815096a4320c119d73e735c4419c473b
hinl204 <86874361+hinl204@users.noreply.github.com>
2021-07-03 17:05:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: 'Yêu em thì click vào đây',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu anh <33',
+    btnNo: 'Không, em gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Bởi vì em quá đáng yêu <33',
+    mess: 'Em biết mà 🥰. Chần chừ gì chồng ơi em đây 😘😘',
+    messDesc: 'Luv U <3.',
+    btnAccept: 'Oki bae <3',
+    messLink:  https://linh.blue.07
 }
